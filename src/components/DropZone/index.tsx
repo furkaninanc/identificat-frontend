ec63994333e81508c2940e7f7430a39a7fe01b35
Furkan INANC <hi@furkaninanc.com>
2021-09-25 19:58:58 +0300

    Add showing cat's image on drop zone

--- src/components/DropZone/index.tsx
+++ src/components/DropZone/index.tsx
@@ -10,17 +10,28 @@ interface IDropZone {
     onSelect: (file: File) => void;
 }
 
+interface IGenerateObjectUrl {
+    (file: File): string;
+}
+
 const DropZone: React.FC<IDropZone> = ({ onSelect }) => {
     const [over, setOver] = useState<boolean>(false);
+    const [image, setImage] = useState<string>();
     const fileRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+
+    const generateObjectUrl: IGenerateObjectUrl = (file) => {
+        return (window.URL || window.webkitURL).createObjectURL(file);
+    };
 
     return (
         <div className={cs(styles.container, over && styles.active)}>
             <input
                 accept="image/*"
                 onChange={() => {
-                    if (fileRef?.current?.files && fileRef.current.files[0])
+                    if (fileRef?.current?.files && fileRef.current.files[0]) {
+                        setImage(generateObjectUrl(fileRef.current.files[0]));
                         onSelect(fileRef.current.files[0]);
+                    }
                 }}
                 className="hidden"
                 ref={fileRef}
@@ -34,7 +45,10 @@ const DropZone: React.FC<IDropZone> = ({ onSelect }) => {
                     event.preventDefault();
                     setOver(false);
 
-                    if (event.dataTransfer.files[0]) onSelect(event.dataTransfer.files[0]);
+                    if (event.dataTransfer.files[0]) {
+                        setImage(generateObjectUrl(event.dataTransfer.files[0]));
+                        onSelect(event.dataTransfer.files[0]);
+                    }
                 }}
                 onDragEnterCapture={() => setOver(true)}
                 onDragLeaveCapture={() => setOver(false)}
@@ -42,7 +56,18 @@ const DropZone: React.FC<IDropZone> = ({ onSelect }) => {
                     event.preventDefault();
                 }}
                 className={styles.dropOverlay}
-            />
+            >
+                {image && (
+                    <img
+                        alt="Your cat"
+                        className={styles.catImage}
+                        src={image}
+                        onError={() => {
+                            setImage(undefined);
+                        }}
+                    />
+                )}
+            </div>
             <div className={styles.background}>
                 <div>
                     <img alt="Drop here" src={DropZoneImage} />
